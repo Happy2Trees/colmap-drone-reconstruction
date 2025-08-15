c3600b12ce2508a2949e21f932b6133acf265e70
qkrwnsdn0427 <qkrwnsdn0427@hanyangac.kr>
2025-08-15 12:49:03 +0000

    update: cotracker_db_generator rename

---
+++ tests/cotracker_npy_save/cotracker_db_generator.sh
@@ -0,0 +1,6 @@
+python conv_sift.py \
+    --npy /home/sanggyun/hdseo/colmap-drone-reconstruction/data/sec2_1_processed_1024x576/cotracker/80_40_sift.npy \
+    --image_dir /home/sanggyun/hdseo/colmap-drone-reconstruction/data/sec2_1_processed_1024x576/images \
+    --K /home/sanggyun/hdseo/colmap-drone-reconstruction/data/sec2_1_processed_1024x576/K.txt \
+    --width 1024 --height 576 \
+    --out_db /home/sanggyun/hdseo/colmap-drone-reconstruction/data/sec2_1_processed_1024x576/outputs/x3_section2_cotracker_sift_test.db
