39e4fa2bda7b60235dbdf6e416516c7a975dd09d
qkrwnsdn0427 <qkrwnsdn0427@hanyangac.kr>
2025-08-18 09:32:01 +0000

    Add : image retrival init

---
+++ tests/image_retrival/match_images.sh
@@ -0,0 +1,5 @@
+python match_images.py \
+--config_path patchnetvlad/configs/performance.ini \
+--image_dir /home/sanggyun/feature_matching/data/sec2_7x_20fps \
+--max_candidates 300
+# --plot_dir match_vis
